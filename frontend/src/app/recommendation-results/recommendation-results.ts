805ab4f3c9acc08873e940ffaa94710653efb8c5
magistrat350 <dixon.john044@gmail.com>
2025-07-04 11:39:04 +0200

    test_recommendation results

--- frontend/src/app/recommendation-results/recommendation-results.ts
+++ frontend/src/app/recommendation-results/recommendation-results.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { SuggestionService } from '../core/services/suggestion.service';
 import { Destinations } from '../core/models/destination';
 import { DestinationPreferences } from '../core/models/travelConfig';
@@ -10,7 +11,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-recommendation-results',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './recommendation-results.html',
   styleUrls: ['./recommendation-results.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,9 +21,12 @@ export class RecommendationResultsComponent {
   destinations: Destinations = history.state.recommendations || [];
   private config: DestinationPreferences = history.state?.config || {};
 
+  loading: boolean = false;
+
   constructor(private router: Router) {}
 
   getDetails(destination: string): void {
+    this.loading = true;
     this.suggestionService
       .getTrip({
         destination: destination,
@@ -30,11 +34,13 @@ export class RecommendationResultsComponent {
       })
       .subscribe({
         next: (trip) => {
+          this.loading = false;
           this.router.navigate(['/trip-details'], {
             state: { trip: trip },
           });
         },
         error: (err) => {
+          this.loading = false;
           console.error('Error fetching trip details:', err);
         },
       });
